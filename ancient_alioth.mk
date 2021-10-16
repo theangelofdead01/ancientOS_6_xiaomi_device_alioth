be6035c4cd67d9b572f719ec168e85f3e6bfe9ac
theangelofdead01 <9635187+theangelofdead01@users.noreply.github.com>
2021-10-16 12:20:34 -0500

    move to ancient

---
+++ ancient_alioth.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/ancient/config/common.mk)
+
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
+
+PRODUCT_NAME := ancient_alioth
+PRODUCT_DEVICE := alioth
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F3
+
+# Inherit some AncientOS stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+ANCIENT_OFFICIAL := true
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.3.0.RKHMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHMIXM:user/release-keys
